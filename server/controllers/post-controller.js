d02c87f2a0ec2405b4314e117c97c5465c1a2ee2
Ankur1231 <ankurudey@gmail.com>
2021-11-16 20:08:00 +0530

    added like/dislike route

--- server/controllers/post-controller.js
+++ server/controllers/post-controller.js
@@ -29,3 +29,74 @@ export const createPost = async (req, res) => {
     res.status(500).json(error);
   }
 };
+
+//getPosts
+export const getPosts = async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.status(200).json(posts);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
+//getPostById
+export const getPostById = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+    if (!post) {
+      return res.status(404).json({ msg: "post not found" });
+    }
+    res.status(200).json(post);
+  } catch (error) {
+    if (error.kind === "ObjectId") {
+      return res.status(404).json({ msg: "post not found" });
+    }
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
+//deletePost
+export const deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+    if (!post) {
+      return res.status(404).json({ msg: "post not found" });
+    }
+    //check user
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "unauthorized user" });
+    }
+    await post.remove();
+    res.status(200).json({ msg: "post removed" });
+  } catch (error) {
+    if (error.kind === "ObjectId") {
+      return res.status(404).json({ msg: "post not found" });
+    }
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
+//likePost
+export const likePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+    if (!post) {
+      return res.status(404).json({ msg: "post not found" });
+    }
+    const index = post.likes.findIndex((value) => value.user == req.user.id);
+    if (index === -1) {
+      await post.likes.push({ user: req.user.id });
+    } else {
+      post.likes.splice(index, 1);
+    }
+    await post.save();
+    res.status(200).json(post);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
--- server/routes/api/post.js
+++ server/routes/api/post.js
@@ -3,7 +3,13 @@ import { check } from "express-validator";
 
 //components
 import checkJwt from "../../middleware/auth.js";
-import { createPost } from "../../controllers/post-controller.js";
+import {
+  createPost,
+  getPosts,
+  getPostById,
+  deletePost,
+  likePost,
+} from "../../controllers/post-controller.js";
 
 const router = express.Router();
 
@@ -15,5 +21,25 @@ router.post(
   [checkJwt, [check("text", "text is required").not().isEmpty()]],
   createPost
 );
+
+//@route  GET api/post
+//@desc   Get all post
+//@access Private
+router.get("/", checkJwt, getPosts);
+
+//@route  GET api/post/:post_id
+//@desc   Get post by ID
+//@access Private
+router.get("/:post_id", checkJwt, getPostById);
+
+//@route  DELETE api/post/:post_id
+//@desc   Delete a post
+//@access Private
+router.delete("/:post_id", checkJwt, deletePost);
+
+//@route  PUT api/post/like/:post_id
+//@desc   like/unlike a post
+//@access Private
+router.put("/like/:post_id", checkJwt, likePost);
 
 export default router;
